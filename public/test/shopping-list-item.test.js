1cb3ba934cc99ded011508c536389b933c522e9e
Rian Mueller <rian@Rians-MacBook-Pro-2.local>
2019-08-01 18:49:31 -1000

    adds first few tests

--- public/test/shopping-list-item.test.js
+++ public/test/shopping-list-item.test.js
@@ -3,7 +3,43 @@ describe("ShoppingListItem", function() {
     expect(ShoppingListItem).to.be.a("class");
   });
 
-  it("should have a property name", function() {
+  it("should have a property named 'name'", function() {
     expect(ShoppingListItem).to.have.ownProperty("name");
   });
+
+  it("should have a property named 'description'", function() {
+    expect(ShoppingListItem).to.have.ownProperty("description");
+  });
+
+  it("should have a property named 'isDone'", function() {
+    expect(ShoppingListItem).to.have.ownProperty("isDone");
+  });
 });
+
+/*
+
+ShoppingListItem has a constructor method that accepts 2 arguments, name and description
+
+the constructor method sets the new instances name and description properties using the arguments passed in
+
+class ShoppingListItem {
+  constructor (name, description) {
+    this.name = name;
+    this.description = description;
+    this.isDone = isDone;
+  }
+
+  check() {
+    isDone = true;
+  }
+
+  uncheck() {
+    isDone = false;
+  }
+
+  render() {
+
+  }
+}
+
+*/
